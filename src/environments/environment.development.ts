266fd9b719616b22d7f0a769766b248258227819
Marc Manuel <marc_manuel@hotmail.com>
2025-10-26 19:50:58 -0400

    Add environment folder commenting it out in ignore file.

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  apiUrl: '/api',
+  webServiceURL: 'https://dotnet-sp-api.onrender.com/api/',
+  appName: 'Sport Profiles',
+  appLogo: '/image/logicon.jpg',
+  appLogoText: 'SportProfiles',
+  companyName: 'MarcMan',
+  webSiteDomain: 'https://www.sportprofiles.space',
+  memberImagesUrlPath: '/images/members/',
+};
